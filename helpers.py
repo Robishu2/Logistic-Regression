a4f007e0e9116ded26d5ef234fb7e14a5c68de43
Robishu2 <Robinb1804@gmail.com>
2025-09-28 18:46:27 +0200

    Added comments and parameters tuning function

--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from logistic_regression import LogisticRegression
 
 # Function to split X and y into a train and test set
 def split_data(X, y, test_size=0.3, random_state=12345, include_intercept=True):
@@ -16,32 +17,80 @@ def split_data(X, y, test_size=0.3, random_state=12345, include_intercept=True):
     # Randomly sample the test set indexes
     idx = y.sample(frac=test_size, random_state=random_state).index
 
+    # Select the train and test sets
     X_train, X_test = X.drop(idx), X.loc[idx]
     y_train, y_test = y.drop(idx), y.loc[idx]
 
     return X_train, X_test, y_train, y_test
 
+# Function to create k folds for cross-validation
 def make_folds(X, folds=5):
+    # Raise an error if the number of folds is invalid
     if folds <= 1:
         raise ValueError('Please ensure that the number of folds >= 2')
 
+    # Randomly shuffle the indexes
     all_idxs = X.index
     all_idxs = np.random.permutation(all_idxs)
+
     idxs_split = []
     num_per_idxs = len(X)//folds
 
     for fold in range(folds):
+        # Select the test fold
         if (folds > 1) & (fold != folds-1):
             idx = all_idxs[fold*num_per_idxs:(fold+1)*num_per_idxs]
         else:
+            # last fold takes the rest
             idx = all_idxs[fold*num_per_idxs:]
 
+        # Build the [test, train] split
         temp = []
         temp.append(idx)
-
         not_in_idxs = [s for s in all_idxs if s not in idx]
         temp.append(not_in_idxs)
 
         idxs_split.append(temp)
 
     return idxs_split
+
+# Function to tune hyperparameters using cross-validation
+def tune(X, y, iter_list, learning_rate_list):
+    best_acc = 0
+    best_specs = {}
+
+    # Function to tune hyperparameters using cross-validation
+    for iterations in iter_list:
+        for learning_rate in learning_rate_list:
+            splits = make_folds(X)
+
+            accs = []
+
+            # Loop over each fold
+            for test_idx, train_idx in splits:
+                # Create train and test sets
+                X_train, X_test = X.loc[train_idx], X.loc[test_idx]
+                y_train, y_test = y.loc[train_idx], y.loc[test_idx]
+
+                # Train logistic regression
+                LR = LogisticRegression()
+                LR.fit_logistic_regression(X_train, y_train, verbose=False)
+
+                # Get predictions
+                y_pred_proba = LR.predict_logistic_regression(X_test)
+                y_pred = (y_pred_proba >= 0.5).astype(int)
+
+                # Compute accuracy
+                acc = np.mean(y_pred == y_test)
+                accs.append(acc)
+
+            # Update best hyperparameters if accuracy improved
+            if np.mean(accs) > best_acc:
+                best_acc = np.round(np.mean(accs), 3)
+                best_specs = {'learning_rate': learning_rate, 'iterations': iterations, 'accuracy': best_acc}
+
+    return best_specs
+
+
+
+
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn.impute import SimpleImputer
 import time
 from logistic_regression import LogisticRegression
-from helpers import split_data, make_folds
+from helpers import split_data, tune
 from xgboost import XGBClassifier
 
 # Start time
@@ -27,66 +27,41 @@ df = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
 y = df['survived']
 X = df.drop(columns=['survived']).copy()
 
-# The code below is comparing the Logistic Regression with XGBoost based on K-Fold Cross-Validation
+# List of potential best number of iterations and learning rate
+iterations_list = [1000, 5000, 10000]
+learning_rate_list = [0.02, 0.03, 0.04, 0.05]
 
-splits = make_folds(X, folds=10)
-
-accuracies_lr = []
-accuracies_xgb = []
-
-for test_idx, train_idx in splits:
-    X_train, X_test = X.loc[train_idx], X.loc[test_idx]
-    y_train, y_test = y.loc[train_idx], y.loc[test_idx]
-
-    # Train the train set
-    LR = LogisticRegression()
-    LR.fit_logistic_regression(X_train, y_train, verbose=False)
-
-    # Get predictions
-    y_pred_proba = LR.predict_logistic_regression(X_test)
-    y_pred = (y_pred_proba >= 0.5).astype(int)
-
-    # Print the accuracy
-    acc = np.mean(y_pred == y_test)
-    accuracies_lr.append(acc)
-
-    # Compare to a basic XGBoost model
-    model = XGBClassifier()
-    model.fit(X_train, y_train)
-    pred = model.predict(X_test)
-
-    # Print the accuracy
-    acc_xgb = np.mean(pred == y_test)
-    accuracies_xgb.append(acc_xgb)
-
-print(f'The accuracy of the Logistic Regression model was: {np.mean(accuracies_lr)}')
-print(f'The accuracy of the XGBoost model was: {np.mean(accuracies_xgb)}')
+# Call the tune function to find the best number of iterations and learning rate
+best_specifications = tune(X, y, iterations_list, learning_rate_list)
+best_iterations = best_specifications['iterations']
+best_learning_rate = best_specifications['learning_rate']
+print(f'The best specifications of the Logistic Regression model are: {best_specifications}')
 
 # The code below is comparing the Logistic Regression with XGBoost based on a train test split
 
 # Get train and test data
-# X_train, X_test, y_train, y_test = split_data(X, y, test_size=0.3, random_state=3)
+X_train, X_test, y_train, y_test = split_data(X, y, test_size=0.3, random_state=3)
 
-# # Train the train set
-# LR = LogisticRegression()
-# LR.fit_logistic_regression(X_train, y_train, verbose=False)
+# Train the train set
+LR = LogisticRegression()
+LR.fit_logistic_regression(X_train, y_train, verbose=False, iterations=best_iterations, learning_rate=best_learning_rate)
 
-# # Get predictions
-# y_pred_proba = LR.predict_logistic_regression(X_test)
-# y_pred = (y_pred_proba >= 0.5).astype(int)
+# Get predictions
+y_pred_proba = LR.predict_logistic_regression(X_test)
+y_pred = (y_pred_proba >= 0.5).astype(int)
 
-# # Print the accuracy
-# acc = np.mean(y_pred == y_test)
-# print(f'The accuracy of the XGBoost model was: {acc}')
+# Print the accuracy
+acc = np.mean(y_pred == y_test)
+print(f'The accuracy of the XGBoost model was: {acc}')
 
-# # Compare to a basic XGBoost model
-# model = XGBClassifier()
-# model.fit(X_train, y_train)
-# pred = model.predict(X_test)
+# Compare to a basic XGBoost model
+model = XGBClassifier()
+model.fit(X_train, y_train)
+pred = model.predict(X_test)
 
-# # Print the accuracy
-# acc_xgb = np.mean(pred == y_test)
-# print(f'The accuracy of the XGBoost model was: {acc_xgb}')
+# Print the accuracy
+acc_xgb = np.mean(pred == y_test)
+print(f'The accuracy of the XGBoost model was: {acc_xgb}')
 
 # End time
 end_time = time.time()
