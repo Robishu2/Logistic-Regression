ede47d57e820c3205edd3787f36a0ed1d271246e
Robishu2 <Robinb1804@gmail.com>
2025-09-28 17:08:21 +0200

    Added the K-Fold Cross-Validation helper function

--- helpers.py
+++ helpers.py
@@ -20,3 +20,28 @@ def split_data(X, y, test_size=0.3, random_state=12345, include_intercept=True):
     y_train, y_test = y.drop(idx), y.loc[idx]
 
     return X_train, X_test, y_train, y_test
+
+def make_folds(X, folds=5):
+    if folds <= 1:
+        raise ValueError('Please ensure that the number of folds >= 2')
+
+    all_idxs = X.index
+    all_idxs = np.random.permutation(all_idxs)
+    idxs_split = []
+    num_per_idxs = len(X)//folds
+
+    for fold in range(folds):
+        if (folds > 1) & (fold != folds-1):
+            idx = all_idxs[fold*num_per_idxs:(fold+1)*num_per_idxs]
+        else:
+            idx = all_idxs[fold*num_per_idxs:]
+
+        temp = []
+        temp.append(idx)
+
+        not_in_idxs = [s for s in all_idxs if s not in idx]
+        temp.append(not_in_idxs)
+
+        idxs_split.append(temp)
+
+    return idxs_split
